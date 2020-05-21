f27bb07f3c7f4f9f5bef2b387cb58060bd913ddd
Monster <coolpest@gmail.com>
2020-05-21 10:43:11 +0800

    更新用户

--- handlers/updateUser.ts
+++ handlers/updateUser.ts
@@ -1,23 +1,24 @@
-import { Request, Response } from "https://deno.land/x/oak/mod.ts";
-import{updateUser} from "../services/users.ts";
-export default async ({params,request,response}:{params:any;request:Request;response:Response})=>{
+import {Request, Response} from "https://deno.land/x/oak/mod.ts";
+import {updateUser} from "../services/users.ts";
+
+export default async ({params, request, response}: { params: any; request: Request; response: Response }) => {
     const userId = params.id
 
     if (!userId) {
         response.status = 400;
-        response.body = { msg: "无效的用户 ID" };
+        response.body = {msg: "无效的用户 ID"};
         return;
     }
     if (!request.hasBody) {
         response.status = 400;
-        response.body = { msg: "用户数据无效" };
+        response.body = {msg: "用户数据无效"};
         return;
     }
 
-    const {
-        value:{name,role,isAdmin}
-    }=await request.body()
+    const d = await request.body()
+    const data = JSON.parse(d.value)
+    const {name, role, isAdmin} = data
 
-    await updateUser(userId,{name,role,isAdmin})
-    response.body = {msg:"更新成功"}
+    await updateUser(userId, {name, role, isAdmin})
+    response.body = {msg: "更新成功"}
 }
